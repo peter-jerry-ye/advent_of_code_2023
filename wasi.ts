12ba072b9686cefea526ad845ad24be5fff85b58
zihang <yezihang@idea.edu.cn>
2023-12-18 14:55:21 +0800

    (m) Cleanup

--- wasi.ts
+++ wasi.ts
@@ -1,36 +1,18 @@
 import Context from "https://deno.land/std@0.206.0/wasi/snapshot_preview1.ts";
-// import { File, OpenFile, WASI } from "npm:@bjorn3/browser_wasi_shim";
 
 const context = new Context({
   args: Deno.args,
   env: Deno.env.toObject(),
 });
 
-// const wasi = new WASI(
-//   Deno.args,
-//   Object.entries(Deno.env.toObject()).map(([key, value]) => key + "=" + value),
-//   [
-//     new OpenFile(new File([])),
-//     new OpenFile(new File([])),
-//     new OpenFile(new File([])),
-//   ],
-// );
 await WebAssembly.instantiateStreaming(
   fetch(
-    // new URL("./wasi.wasm", import.meta.url),
     new URL("./target/wasm-gc/release/build/main/main.wasm", import.meta.url),
   ),
   {
     "wasi_snapshot_preview1": context.exports,
   },
-  // { "wasi_snapshot_preview1": wasi.wasiImport },
 ).then((obj) => {
-  // wasi.start({
-  //   exports: {
-  //     memory: obj.instance.exports["moonbit.memory"] as WebAssembly.Memory,
-  //     _start: obj.instance.exports["_start"] as () => unknown,
-  //   },
-  // // });
   try {
     context.start(obj.instance);
   } catch (e) {
